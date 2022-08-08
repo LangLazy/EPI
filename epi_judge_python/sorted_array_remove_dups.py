f38d424523892abca2b0f5837c8b89bc7c0321d0
Rahul <rahul.gudise@gmail.com>
2022-08-08 10:17:55 -0400

    did remove dups problem

--- epi_judge_python/sorted_array_remove_dups.py
+++ epi_judge_python/sorted_array_remove_dups.py
@@ -7,8 +7,14 @@ from test_framework.test_utils import enable_executor_hook
 
 # Returns the number of valid entries after deletion.
 def delete_duplicates(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    toUse = 1
+    for i in range(1,len(A),1):
+        if A[toUse-1] == A[i]:
+            continue
+        else:
+            A[toUse] = A[i]
+            toUse += 1
+    return toUse
 
 
 @enable_executor_hook
