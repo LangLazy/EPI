f15b8d3b4b3507d127a0735f5af000f88ab6196c
Rahul <rahul.gudise@gmail.com>
2022-08-09 11:43:37 -0400

    did look and say

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -2,8 +2,25 @@ from test_framework import generic_test
 
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    def countOcur(s, pos) -> int:
+        start = s[pos]
+        while pos < len(s) and s[pos] == start:
+            pos += 1
+        return pos #the first diff spot
+    def update(s) -> str:
+        cur = 0
+        ans = ""
+        while cur < len(s):
+            currentLet = s[cur]
+            endPos = countOcur(s, cur)
+            ans += str(endPos - cur)
+            ans += currentLet
+            cur = endPos
+        return ans
+    ans = "1"
+    for i in range(n-1):
+        ans = update(ans)
+    return ans
 
 
 if __name__ == '__main__':
