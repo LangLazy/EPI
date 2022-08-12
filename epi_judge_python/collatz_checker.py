bcffa177d385b5f1c48c371933c10c1723aaa8fc
Rahul <rahul.gudise@gmail.com>
2022-08-12 15:58:11 -0400

    i proved the collatz conjecture fr

--- epi_judge_python/collatz_checker.py
+++ epi_judge_python/collatz_checker.py
@@ -1,9 +1,30 @@
+from ast import main
 from test_framework import generic_test
 
 
 def test_collatz_conjecture(n: int) -> bool:
-    # TODO - you fill in here.
-    return False
+    mainSet = set()
+    def runCol(n):
+        nonlocal mainSet
+        if n in mainSet:
+            return True
+        curSet = set()
+        while n != 1:
+            curSet.add(n)
+            mainSet.add(n)
+            if n%2 == 0:
+                n /= 2
+            else:
+                n = n*3 + 1
+            if n in curSet:
+                return False
+            elif n in mainSet:
+                return True
+        return True
+    for i in range(1,n+1):
+        if not runCol(i):
+            return False
+    return True
 
 
 if __name__ == '__main__':
